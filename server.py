fe2befb2b2d3eb2cca83373d6669e7f9e3511b3f
Chris Soukup <csoukup33@gmail.com>
2018-01-02 22:39:36 -0600

    Added preliminary slot read function

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
@@ -8,22 +8,56 @@ db_connect = create_engine('sqlite:///test.db')
 app = Flask(__name__)
 api = Api(app)
 
+#Route_1
 class Rooms(Resource):
     def get(self):
         conn = db_connect.connect() # connect to database
         query = conn.execute("select * from rooms") # This line performs query and returns json result
         return {'rooms': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is Room ID
 
+#Route_2
 class Room(Resource):
     def get(self, roomId):
         conn = db_connect.connect()
-        query = conn.execute("select * from rooms where roomId =%d " %int(roomId))
+        query = conn.execute("select * from rooms where roomNum =%d " %int(roomId))
+        result = { 'data': [dict(zip(tuple (query.keys()), i)) for i in query.cursor]}
+        return jsonify(result)
+
+#Route_3
+class Read(Resource):
+    def get(self, roomId, slotId, field):
+        conn = db_connect.connect()
+        if field == 'name' and int(slotId) >= 1 and int(slotId) <= 999:
+            #return slot name
+            slot = 'slot'
+            slot += str(slotId)
+            slot += '_name'
+            print slot
+            query = conn.execute('select %s from rooms where roomNum =%d' %(slot, int(roomId)))
+        elif field == 'value' and int(slotId) >= 1 and int(slotId) <= 999:
+            #return slot value
+            slot = 'slot'
+            slot += str(slotId)
+            slot += '_value'
+            query = conn.execute('select %s from rooms where roomNum =%d' %(slot, int(roomId)))
+        elif field == 'units' and int(slotId) >= 1 and int(slotId) <= 999:
+            #return slot units
+            slot = 'slot'
+            slot += str(slotId)
+            slot += '_units'
+            query = conn.execute('select %s from rooms where roomNum =%d' %(slot, int(roomId)))
+        else:
+            #return error
+            #return {'error'}
+            abort(404)
+
         result = { 'data': [dict(zip(tuple (query.keys()), i)) for i in query.cursor]}
         return jsonify(result)
 
 api.add_resource(Rooms, '/rooms') # Route_1
-api.add_resource(Room, '/rooms/<roomId>') # Route_3
-
+api.add_resource(Room, '/rooms/<roomId>') # Route_2
+api.add_resource(Read, '/rooms/<roomId>/<slotId>/read/<field>') # Route_3
+#api.add_resource(Write, '/rooms/<roomId>/<slotId>/write/<field>/<value>') # Route_4
 
 if __name__ == '__main__':
-    app.run(port='8002')
+    app.run(port=8002)
