e985b5ea6e97dc8b621598cc5b12a62b7f1d3acf
Chris Soukup <csoukup33@gmail.com>
2018-01-17 06:05:47 -0600

    Added initial write functionality

--- server.py
+++ server.py
@@ -8,6 +8,12 @@ db_connect = create_engine('sqlite:///test.db')
 app = Flask(__name__)
 api = Api(app)
 
+#New error response handler
+def bad_request(message):
+    response = jsonify({'message': message})
+    response.status_code = 400
+    return response
+
 #Route_1
 class Rooms(Resource):
     def get(self):
@@ -34,18 +40,24 @@ class Read(Resource):
             slot += '_name'
             print slot
             query = conn.execute('select %s from rooms where roomNum =%d' %(slot, int(roomId)))
+            for row in query:
+              result = row[slot]
         elif field == 'value' and int(slotId) >= 1 and int(slotId) <= 999:
             #return slot value
             slot = 'slot'
             slot += str(slotId)
             slot += '_value'
             query = conn.execute('select %s from rooms where roomNum =%d' %(slot, int(roomId)))
+            for row in query:
+              result = row[slot]
         elif field == 'units' and int(slotId) >= 1 and int(slotId) <= 999:
             #return slot units
             slot = 'slot'
             slot += str(slotId)
             slot += '_units'
             query = conn.execute('select %s from rooms where roomNum =%d' %(slot, int(roomId)))
+            for row in query:
+              result = row[slot]
         else:
             #return error
             #return {'error'}
@@ -54,10 +66,44 @@ class Read(Resource):
         result = { 'data': [dict(zip(tuple (query.keys()), i)) for i in query.cursor]}
         return jsonify(result)
 
+
+#Route_3
+class Write(Resource):
+    def get(self, roomId, slotId, field, value):
+        conn = db_connect.connect()
+        ## Check that room is in the valid int range
+        if int(roomId) >=1 and int(roomId) <=999:
+            if field == 'value' or field == 'name' or field == 'units':
+                if int(slotId) >= 1 and int(slotId) <= 12:
+                    slot = 'slot'
+                    slot += str(slotId)
+                    if field == 'value':
+                        slot += '_value'
+                    elif field == 'name':
+                        slot += '_name'
+                    else:
+                        slot += '_units'
+                    updatequery = conn.execute('update rooms set %s = \'%s\' where roomNum =%d' %(slot, value, int(roomId)))
+                    lookupquery = conn.execute('select %s from rooms where roomNum =%d' %(slot, int(roomId)))
+                    for row in lookupquery:
+                        #result = row[slot]
+                        #result = { 'data': [dict(zip(tuple (lookupquery.keys()), i)) for i in lookupquery.cursor]}
+                        result = { 'Data written to slot': row[slot] }
+                        return jsonify(result)
+                else:
+                    return bad_request('Slot number out of range: ' + slotId)
+            else:
+                #print ('Invalid field: ' + field)
+                #abort(404)
+                return bad_request('Invalid field: ' + field)
+        else:
+            return bad_request('Room number out of range: ' + roomId)
+
+
 api.add_resource(Rooms, '/rooms') # Route_1
 api.add_resource(Room, '/rooms/<roomId>') # Route_2
 api.add_resource(Read, '/rooms/<roomId>/<slotId>/read/<field>') # Route_3
-#api.add_resource(Write, '/rooms/<roomId>/<slotId>/write/<field>/<value>') # Route_4
+api.add_resource(Write, '/rooms/<roomId>/<slotId>/write/<field>/<value>') # Route_4
 
 if __name__ == '__main__':
     app.run(port=8002)
